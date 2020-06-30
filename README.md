cf790b68b3c88b4f7ffd21bab5ba60f070d7b02e
cbping <452775680@qq.com>
2020-06-30 17:28:56 +0800

    feat: pvc-nfs

--- README.md
+++ README.md
@@ -14,9 +14,12 @@ docker build -t cbping/gok8s:1.0 .
 # kubectl create configmap gok8s-config --from-file=./conf/config.json
 kubectl create  -f configmap.yaml
 
+# 创建pvc
+# 可能需要安装nfs包 yum -y install  nfs-utils rpcbind
+kubectl apply  -f pvc-nfs.yaml
+
 # 启动ingress-controller
 # - minikube: minikube addons enable ingress
-# 
 
 # k8s部署
 kubectl apply -f gok8s-demo.yaml
@@ -27,3 +30,8 @@ kubectl get ingress
 # 访问
 http://{$ingress_address}/(ping|version)
 ```
+
+
+
+
+      
--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 type Config struct {
@@ -42,10 +41,24 @@ func versionHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"version": config.Version})
 }
 
+func fileHandler(c *gin.Context) {
+	data := c.GetString("content")
+	if data == "" {
+		data = "hello world [pv]"
+	}
+	err := ioutil.WriteFile("./pv/output.txt", []byte(data), 0666)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": data})
+	}
+}
+
 func main() {
 	router := gin.New()
 	router.Use(gin.Recovery())
 	router.GET("/ping", statusOKHandler)
 	router.GET("/version", versionHandler)
+	router.GET("/file", fileHandler)
 	router.Run(config.Addr)
 }
